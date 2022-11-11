0cf15b1f9b8f8782ebf0585fe5b8dc5b860f15ec
melekmoalla <melekmoalla2468@gmail.com>
2022-11-11 14:00:32 +0100

    yes i do it

--- function.c
+++ function.c
@@ -7,7 +7,7 @@
 int _printf(const char *format, ...)
 {
 	op_t ops[] = {{'c', printf_char}, {'s', printf_st}, {'%', printf_persent},
-	{'d', printf_de}, {'i', printf_de}, {'R', printf_rev}};
+	{'d', printf_de}, {'i', printf_de}, {'r', printf_rev}};
 	int i = 0, z = 0, b = 0, len = 0;
 	va_list args;
 
